cd1a988d3519a92e152ae69a4ee6a8bcf471a420
Sydnee Spruiell <sydneespruiell@sydnees-mbp.attlocal.net>
2017-01-11 20:38:38 -0600

    ejeldrid: Assign button function and link labels/cards

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageView: UIImageView!
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,13 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func dealTapped(_ sender: UIButton) {
+        print("Deal tapped")
+        
+        leftImageView.image = UIImage(named: "card10")
+        rightImageView.image = UIImage(named: "jack")
     }
 
 
