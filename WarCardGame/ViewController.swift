7d26829e3c808ac54e3470b2628f5547336942f0
Sydnee Spruiell <sydneespruiell@sydnees-mbp.attlocal.net>
2017-01-11 23:03:39 -0600

    ejeldrid: App finished. Added card comparison and functional scoring

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var leftImageView: UIImageView!
-    @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var leftScoreLabel: UILabel!
+    var leftScore = 0
+    
+    @IBOutlet weak var rightImageView: UIImageView!
     @IBOutlet weak var rightScoreLabel: UILabel!
+    var rightScore = 0
+    
+    let cardNames = ["ace", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "jack", "queen", "king"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,10 +32,38 @@ class ViewController: UIViewController {
     }
 
     @IBAction func dealTapped(_ sender: UIButton) {
-        print("Deal tapped")
         
-        leftImageView.image = UIImage(named: "card10")
-        rightImageView.image = UIImage(named: "jack")
+        //Randomize left number from 0-12
+        let leftNumber = Int(arc4random_uniform(13))
+        
+        //Set the left image
+        leftImageView.image = UIImage(named: cardNames[leftNumber])
+
+        //Randomize right number from 0-12
+        let rightNumber = Int(arc4random_uniform(13))
+        
+        //Set the right image
+        rightImageView.image = UIImage(named: cardNames[rightNumber])
+        
+        //Compare the card numbers
+        if leftNumber > rightNumber {
+            //Left card wins
+            leftScore += 1
+            
+            //Update the label
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber == rightNumber {
+            //Tie
+        }
+        else {
+            //Right card wins
+            rightScore += 1
+            
+            //Update the label
+            rightScoreLabel.text = String(rightScore)
+        }
+        
     }
 
 
